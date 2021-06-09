eb30f813f7336d60bf69fc4f5843e182426b9a2f
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2021-06-09 08:57:11 -0300

    lock squares if they already filled or if the game is ended

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
 
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
